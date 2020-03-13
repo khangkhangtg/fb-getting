3f162ac365ebb8bddb7589a5c93d1f7802c1efd9
khangkhangtg <khangkhangtg@gmail.com>
2020-03-13 14:29:54 +0700

    inital commit

---
+++ scriptFB.js
@@ -0,0 +1,7 @@
+// Remove Profile timeline footer pagelet (language, footer, ...)
+const a = document.getElementById("profile_timeline_rhs_footer_pagelet");
+a.parentNode.removeChild(a);
+
+// Remove Chat sidebar
+const chat = document.querySelectorAll('');
+
