515353e6f59a51fac9c753a02e72760ad2d6e77a
Kit Hoang <khangkhangtg@gmail.com>
2020-03-13 22:30:10 +0700

    add new method for new facebook

--- scriptFB.js
+++ scriptFB.js
@@ -5,3 +5,16 @@ a.parentNode.removeChild(a);
 // Remove Chat sidebar
 const chat = document.querySelectorAll('');
 
+// New Facebook
+const spans = document.getElementsByTagName('span');
+const spanInnerText = Array.from(spans).map(el => el.innerText);
+
+const STRING = 'Quyền riêng tư · Điều khoản · Quảng cáo · Lựa chọn quảng cáo  · Cookie · Xem thêm · Facebook © 2020';
+
+let footer_pagelet;
+
+spanInnerText.forEach((el, id) => {
+	if(el === STRING) footer_pagelet = spans[id];
+});
+
+console.log(footer_pagelet);
